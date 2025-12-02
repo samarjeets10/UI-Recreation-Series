503f37d8d826a1f0e4f1f649d1b7018ceca265ad
Samarjeet <samarsabale1021@gmail.com>
2025-12-02 22:36:39 +0530

    feat: implement pricing card component with styles and layout adjustments

--- Pricing-card-ui/src/App.jsx
+++ Pricing-card-ui/src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import './index.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div>
-      <h1>Pricing plans</h1>
-      <p>choose the right plan for your needs.</p>
+      <div className='wrapper'>
+
+        <div className="title">
+          <h1>Pricing plans</h1>
+          <p>Choose the right plan for your needs.</p>
+        </div>
+
+        <div className="card-container">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+
+      </div>
+
     </div>
   )
 }
---
+++ Pricing-card-ui/src/components/Card.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Card.css';
+
+function Card() {
+  return (
+    <div>
+        <div className="card">
+
+            <div className="inner-card">
+                <div className="price-section">
+                    <div className="tag">
+                        <button>Starter</button>
+                    </div>
+                    <div className="price">
+                        <h1>$0<span>/month</span></h1>
+                    </div>
+                </div>
+                <div className="detail-section">
+                    <p>Perfect for small Teams</p>
+                    <button>Start Hiring</button>
+                </div>
+            </div>
+
+            <div className="card-features">
+                <ul>
+                    <li className='feature'>
+                        <div className='check-icon'><i class="ri-check-line"></i></div>
+                        <p>3 Projects</p>
+                    </li>
+                    <li className='feature'>
+                        <div className='check-icon'><i class="ri-check-line"></i></div>
+                        <p>AI Applicant Screening</p>
+                    </li>
+                    <li className='feature'>
+                        <div className='check-icon'><i class="ri-check-line"></i></div>
+                        <p>AI Recruiter</p>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Card
