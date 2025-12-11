87e31dd9ace8c90e0cc721341e917925d9d35006
Samarjeet <samarsabale1021@gmail.com>
2025-12-11 13:01:21 +0530

    feat: update Signin Page layout and styles with new form section and improved responsiveness

--- Signin_page_ui/src/App.jsx
+++ Signin_page_ui/src/App.jsx
@@ -18,11 +18,15 @@ function App() {
             <div className="left_wrapper">
               <div className='text_content'>
                 <div className="header">
-                  <div className='image'><img src={Plane} alt="img" /></div>
+                  <div className='logo'><img src={Plane} alt="img" /></div>
                 </div>
                 <div className="text_section">
+
                   <h1>Welcome back!</h1>
                   <p>Your work, your team, your flow — all in one place</p>
+
+                
+                  {/* Sign in with google or apple*/}
                   <div className='signin_with'>
                     <div className='btn'>
                       <div>
@@ -37,14 +41,47 @@ function App() {
                       <p>Sign In with Apple</p>
                     </div>
                   </div>
+
+
+                  {/* Or block */}
+                  <div className='or'>
+                    <div className='line'>
+                      <span>Or</span>
+                    </div>
+                  </div>
+
+
+                  {/* form section */}
+                  <div className="form_section">
+                    <form>
+                      <input type="text" placeholder='Enter your email' />
+                      <button>Sign in with email</button>
+                    </form>
+                    <p>
+                      Don't have an account? <a>Sign up</a>
+                    </p>
+                  </div>
+
+
                 </div>
+
+
+                {/* Bottom Section */}
                 <div className="bottom_section">
-                  <p>Home</p>
+                  <div className="menu">
+                    <div className='opt home'><p>Home</p></div>
+                    <div className='opt term'><p>Terms</p></div>
+                    <div className='opt'><p>Privacy</p></div>
+                  </div>
                 </div>
               </div>
             </div>
+
+
+            {/* Write hero Wrapper */}
             <div className="right_wrapper">
             </div>
+
           </div>
         </div>
       </div>
