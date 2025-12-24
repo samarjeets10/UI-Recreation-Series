b61f0d71b6a303311cca6d649d0f17b93291665e
Samarjeet <samarsabale1021@gmail.com>
2025-12-24 22:34:38 +0530

    feat: enhance Fashion Hero Page UI with new layout, search functionality, and responsive design adjustments for the navigation bar

--- Fashion_hero_page_ui/src/App.jsx
+++ Fashion_hero_page_ui/src/App.jsx
@@ -23,14 +23,37 @@ function App() {
 
             <div className="search_media">
               <div className="search-bar">
-                <p>Search</p>
+                <i class="ri-search-line"></i>
+                <input type="text" placeholder='Search' />
               </div>
-              <p>Like</p>
-              <p>mail</p>
+              <i class="ri-heart-3-line"></i>
+              <i class="ri-mail-unread-line"></i>
             </div>
           </div>
         </nav>
+
+
         <section className="hero">
+
+          <div className="hero-left">
+            <div className="hero_title_box">
+
+              <div className='top'>
+                <h1>FOR <span><i class="ri-arrow-right-long-line"></i></span> <br />
+                 EVERYONE BUT <br />
+                 NOTANYONE
+                </h1>
+              </div>
+              <div className="bottom">
+                <p>We establish personal relationship with our boutiques, to make sure each is vetted for a stress-free shopping experience</p>
+              </div>
+
+            </div>
+          </div>
+
+          <div className="hero_right">
+
+          </div>
 
         </section>
       </div>
