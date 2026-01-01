5f746f8f04f623fb7c676e099e9d4b5bfc3b6cee
Samarjeet <samarsabale1021@gmail.com>
2026-01-01 23:04:57 +0530

    feat: initialize post_composer_ui with React and Vite
    - Create main application component (App.jsx) and associated styles (App.css)
    - Implement Card component (Card.jsx) with basic structure and styles (Card.css)
    - Set up global styles in index.css, including Google Fonts and layout styles

---
+++ post_composer_ui/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./index.css"
+import Card from './components/Card'
+
+function App() {
+  return (
+    <>
+      <div className="wrapper">
+          <div className="container">
+            <h1>Card</h1>
+            <Card />
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ post_composer_ui/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Card.css"
+
+function Card() {
+  return (
+    <div>
+      <div className="card">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Card
