23252f62c5fd291bd77bde085b2330fd84f9cf94
Samarjeet <samarsabale1021@gmail.com>
2026-01-02 23:32:21 +0530

    feat: initialize profile_card_ui with React and Vite setup
    - Created App component with basic structure and Card component
    - Added CSS files for styling the application

---
+++ profile_card_ui/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css'
+import Card from './components/Card'
+
+function App() {
+  return (
+    <>
+      <div className="wrapper">
+        <h1>Tera Rasta mai na chodu</h1>
+        <Card />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ profile_card_ui/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Card.css'
+
+function Card() {
+  return (
+    <div>
+      <h2>Card</h2>
+    </div>
+  )
+}
+
+export default Card
