96acc2b5ea85f57a98bf1a381a3a6fd022e46c0e
Samarjeet <samarsabale1021@gmail.com>
2026-01-04 13:29:44 +0530

    feat: enhance Card component with dynamic styles and layout adjustments, add cover background image, and update App component to utilize new styles.

--- profile_card_ui/src/App.jsx
+++ profile_card_ui/src/App.jsx
@@ -3,10 +3,42 @@ import './index.css'
 import Card from './components/Card'
 
 function App() {
+
+  const style = [
+
+    {
+      profile_img: 'https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D',
+      background: '#FFFFFF',
+      box_clr: '#F3F6FB',
+      text: '#000',
+      gray_text: '#B7B7B7',
+      btn_text: '#FFFFFF',
+      btn_bg: '#000'
+    },
+
+    {
+      profile_img: 'https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D',
+      background: '#000000',
+      box_clr: '#363636',
+      text: '#FFFFFF',
+      sec_text: '#FFFFFF',
+      gray_text: '#B7B7B7',
+
+      btn_text: '#000',
+      btn_bg: '#FFFFFF'
+    }
+  ];
+
+
   return (
     <>
       <div className="wrapper">
-        <Card />
+
+        {style.map((elem) => {
+            return <Card profile_img={elem.profile_img} background={elem.background} box_clr={elem.box_clr} text={elem.text} gray_text={elem.gray_text} btn_bg={elem.btn_bg} btn_text={elem.btn_text} />
+          })
+        }
+
       </div>
     </>
   )
--- profile_card_ui/src/components/Card.jsx
+++ profile_card_ui/src/components/Card.jsx
@@ -1,47 +1,47 @@
 import React from 'react'
 import './Card.css'
 
-function Card() {
+function Card({ profile_img,background, box_clr, text, gray_text, btn_bg, btn_text}) {
   return (
     <div>
-      <div className="card">
+      <div className="card" style={{background: background}}>
 
         <div className="cover_image">
             <div className="profile_photo">
-                <img src="https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTI2fHxwb3J0cmFpdHxlbnwwfHwwfHx8MA%3D%3D" alt="profile_photo" />
+                <img src={profile_img} alt="profile_photo" />
             </div>
         </div>
 
         <div className="user_handel">
 
             <div className="user_info">
-                <h2>Roman Rouf Col.</h2>
-                <p>Gym Expert</p>
+                <h2 style={{color: text}}>Roman Rouf Col.</h2>
+                <p style={{color: gray_text}}>Gym Expert</p>
             </div>
-            <div className="status">
+            <div className="status" style={{background: box_clr}}>
                 <div className="indecator"></div>
-                <p>Online</p>
+                <p style={{color: gray_text}} >Online</p>
             </div>
 
         </div>
 
-        <div className="user_exp">
+        <div className="user_exp" style={{background: box_clr}}>
             <div className="box">
-                <h2>4.5</h2>
-                <p>Rating</p>
+                <h2 style={{color: text}}>4.5</h2>
+                <p style={{color: gray_text}}>Rating</p>
             </div>
             <div className="box">
-                <h2>200</h2>
-                <p>Hours</p>
+                <h2 style={{color: text}}>200</h2>
+                <p style={{color: gray_text}}>Hours</p>
             </div>
             <div className="box">
-                <h2>06</h2>
-                <p>Months</p>
+                <h2 style={{color: text}}>06</h2>
+                <p style={{color: gray_text}}>Months</p>
             </div>
         </div>
 
         <div className="cta_section">
-            <button className='btn'>Get In Touch</button>
+            <button className='btn' style={{background: btn_bg, color: btn_text}}>Get In Touch</button>
         </div>
 
       </div>
