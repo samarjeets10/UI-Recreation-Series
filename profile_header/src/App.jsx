639931b7ece5dc442cf95293ad46a922c22f9b11
Samarjeet <samarsabale1021@gmail.com>
2026-01-22 23:54:40 +0530

    feat: initialize profile_header with React and Vite setup, create Card component and associated styles

---
+++ profile_header/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+import Card from './components/Card'
+
+function App() {
+  return (
+    <>
+      
+      <div className="wrapper">
+        <Card />
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ profile_header/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Card.css'
+
+function Card() {
+  return (
+    <div>
+
+      <div className="main__container">
+          
+      </div>
+      
+    </div>
+  )
+}
+
+export default Card
