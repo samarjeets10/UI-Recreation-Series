807620c09ea86df76460ad2e7cab7ec10e750680
Samarjeet <samarsabale1021@gmail.com>
2026-04-06 23:36:41 +0530

    feat: initialize Verdant project with React and Vite
    - Create App.css for styling the main application
    - Implement App.jsx as the main component of the application
    - Add Hero_image.jpg as a background image
    - Create index.css for global styles
    - Set up main.jsx to render the App component
    - Add components folder for seperate ui components rendering
    - styled main wrapper hero landing page.
    - enhanced style for minimal navigation bar.

---
+++ Verdant/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div className="wrapper">
+        <div className="main__container">
+          <nav className="nav__container">
+            <div className="logo__box">
+              <h1>Verdant</h1>
+            </div>
+            <div className="nav__bar">
+
+            </div>
+            <div className="cta__container">
+              <button>login</button>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
